832a8aa25bcef22f994fbec16bad3bfe4058f818
Michael Gissing <michael@gissing.at>
2020-05-06 15:20:38 +0200

    project hello world

--- src/main/java/io/github/scolytus/App.java
+++ src/main/java/io/github/scolytus/App.java
@@ -1,10 +1,19 @@
 package io.github.scolytus;
 
-/**
- * Hello world!
- */
+import com.google.common.base.Stopwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOGGER.info("App started");
+        final Stopwatch stopwatch = Stopwatch.createStarted();
+
+        LOGGER.info("DONE after {}ms", stopwatch.elapsed(TimeUnit.MILLISECONDS));
     }
 }
